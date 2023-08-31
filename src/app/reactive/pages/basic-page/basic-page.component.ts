aab76abd05580e5b70a4b761534c02367eb15431
Your Name <yefferson.barrial@avivel.com.pe>
2023-08-30 22:08:38 -0500

    add basic compoennt

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage:[0, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    if(this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+    
+  }
 
 }
