891251199cdfad0bf57631108ad967e03fb506bb
Your Name <yefferson.barrial@avivel.com.pe>
2023-09-17 17:01:27 -0500

    custom validations

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from 'src/app/shared/Validators/validators.functions';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  constructor(private fb: FormBuilder) { }
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  isValidField(field: string) {
+    // TODO: obtener validación desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesPageComponent {
+
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  //ngSubmit
+  onSave() {
+    if( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
+
 
 }
