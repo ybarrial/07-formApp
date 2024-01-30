1fb462e1c84b56d4e70351e75602095474af2f77
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-30 15:54:51 -0500

    restructuring formGroup, add validatorService in form Controls

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from 'src/app/shared/Validators/validators.functions';
+import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,22 +10,32 @@ import * as customValidators from 'src/app/shared/Validators/validators.function
 })
 export class RegisterPageComponent {
 
-  constructor(private fb: FormBuilder) { }
-
-  public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
-    username: ['', [Validators.required, customValidators.cantBeStrider]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
+  myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )  ]],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
+    //email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ this.emailValidator ]],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )]],
+    username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo('password','password2')
+    ]
   });
 
-  isValidField(field: string) {
-    // TODO: obtener validación desde un servicio
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    //private emailValidator: EmailValidator
+  ) {}
+
+  isValidField( field: string ) {
+    return this.validatorsService.isValidField( this.myForm, field );
   }
 
   onSubmit() {
     this.myForm.markAllAsTouched();
   }
-
 }
