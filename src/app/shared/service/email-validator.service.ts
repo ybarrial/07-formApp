f7d5a4f080f9f74991916acf065001b12ffb27c8
Your Name <yefferson.barrial@avivel.com.pe>
2024-01-31 17:26:01 -0500

    validate asyncronous

--- src/app/shared/service/email-validator.service.ts
+++ src/app/shared/service/email-validator.service.ts
@@ -9,46 +9,46 @@ import { delay, Observable, of } from 'rxjs';
 export class EmailValidator implements AsyncValidator {
 
 
-  // validate(control: AbstractControl ): Observable<ValidationErrors | null> {
-
-  //   const email = control.value;
-
-  //   const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
-
-  //     console.log({ email });
-
-  //     if ( email === 'fernando@google.com' ) {
-  //       subscriber.next({ emailTaken: true });
-  //       subscriber.complete();
-  //       // return;
-  //     }
-
-  //     subscriber.next(null);
-  //     subscriber.complete();
-
-
-  //   }).pipe(
-  //     delay( 3000 )
-  //   );
-
-  //   return httpCallObservable;
-
-  // }
-
-
-
   validate(control: AbstractControl ): Observable<ValidationErrors | null> {
 
     const email = control.value;
-    console.log({ email })
 
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors | null>( (subscriber) => {
+
+      console.log({ email });
+
+      if ( email === 'yefferson@google.com' ) {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        // return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+
     }).pipe(
-      delay( 2000 )
+      delay( 3000 )
     );
 
+    return httpCallObservable;
+
   }
+
+
+
+  // validate(control: AbstractControl ): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+  //   console.log({ email })
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay( 2000 )
+  //   );
+
+  // }
 
 
 }
